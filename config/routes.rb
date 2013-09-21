54391c2518bddd488ddc46ffa7fca31ca78635e5
edymerchk <edyhack@gmail.com>
2013-09-21 18:32:20 -0500

    basic form for search

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Camaras::Application.routes.draw do
   get "static/about"
   get '/camaras/:id', to: 'camaras#show', as: 'camara'
   get '/camaras', to: 'camaras#index'
+  get '/search', to: 'camaras#index'
   root to: 'camaras#index'
 end
